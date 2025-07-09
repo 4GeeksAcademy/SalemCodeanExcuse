e17b4ae71cdfc8572474b93501c1ebd7c08373dd
salemmohdmohd <salim.farouq@gmail.com>
2025-07-09 10:44:09 -0500

    simple func

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog ", "My friend ", "Batman ", "My Cat "];
+let action = ["ate ", "sold ", "lost ", "broke "];
+let what = ["my homework "];
+let when = [
+  "before I woke up",
+  "while I was reading",
+  "while I was coding",
+  "during my lunch break",
+  "while I was playing ukulele",
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+//write your code here
+document.getElementById("excuse").innerHTML =
+  getRandom(who) + getRandom(action) + getRandom(what) + getRandom(when);
